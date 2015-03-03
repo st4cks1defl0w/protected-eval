e37ed1990cc5bc291bdaccfe288902b56ac61d10
stacksideflow <1@stacksideflow.host>
2015-03-03 18:55:54 +0100

    Refactor to a separate skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to protected-eval
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/protected_eval/core_test.clj
@@ -0,0 +1,7 @@
+(ns protected-eval.core-test
+  (:require [clojure.test :refer :all]
+            [protected-eval.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
