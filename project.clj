17715b092253a19e7f7554ea33811ba7943f1837
stacksideflow <1@stacksideflow.host>
2015-03-03 19:56:48 +0100

    Migrate to a separate ns

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject protected-eval "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+(defproject protected-eval "0.1.0"
+  :description "  "
+  :url "https://stacksideflow.host"
+  :license {:name "GNU GPU v 3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [lein-cljfmt "0.6.4"]
+                 [jonase/eastwood "0.3.5"]
+                 [lein-bikeshed "0.5.1"]]
   :main ^:skip-aot protected-eval.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/protected_eval/core.clj
+++ src/protected_eval/core.clj
@@ -1,7 +1,70 @@
-(ns protected-eval.core
-  (:gen-class))
+(ns spachat.protected-eval)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defonce
+  ^{:doc "Keeping track of active REPL connections - will be useful for
+         future full-fledged ARI authing."}
+  *active-repls
+  (atom #{}))
+
+(defmacro defnremote [name & rest]
+    `(defn ~(vary-meta name assoc :remote-eval true) ~@rest))
+
+(defn- defnremote? [code]
+  (->> code
+       clojure.edn/read-string
+       first
+       symbol
+       resolve
+       meta
+       :remote-eval))
+
+(defn eval-apply-remote-only
+  "nREPL middleware with DROP-default policy. Allows only
+   evalutaion of defnremote'd functions, other evals are converted to nil.
+   *active-repls count state is currently necessary to whitelist
+   nREPL internal startup calls"
+  [h]
+  (fn [{:keys [code op session] :as msg}]
+    (when (some? session)
+        (reset! *active-repls (conj @*active-repls session)))
+    (if (or (>= 2 (count @*active-repls))
+            (and (= op "eval") (defnremote? code)))
+      (h msg)
+      (h (assoc msg :code "nil")))))
+
+(def ^:private
+  whitelisted-ops-cider
+  #{"classpath" "clone" "describe" "eldoc" "close" "interrupt"})
+
+(defn eval-apply-remote-only-cider
+  "Same as eval-apply-remote-only, but works with Emacs CIDER REPL (eval in repl buffer)
+  Less secure. Following nREPL operations are allowed
+  (necessary for minimal CIDER repl connection):
+  - classpath:
+  return a sequence of File objects of elements on the classpath.
+  - clone
+  initiates (copies) a new session, nothing is disclosed
+  - describe
+  transmits a collection of available nREPL operations
+  (lists all of them, disregarding nrepl-protected limitations)
+  - close
+  closes nREPL connection
+  - interrupt
+  interrupts nREPL connection "
+  [h]
+  (fn [{:keys [code op session] :as msg}]
+    (when (some? session)
+      (reset! *active-repls (conj @*active-repls session)))
+    (if (or (contains? whitelisted-ops-cider op)
+            (>= 2 (count @*active-repls))
+            (and (= op "eval") (defnremote? code)))
+      (h msg)
+      (h (assoc msg :code "nil")))))
+
+(defnremote eval-me-with-nrepl []
+  (str "some useful info about the server, like current time: "
+           (quot (System/currentTimeMillis) 1000)))
+
+(defn do-not-eval-me-with-nrepl []
+  (str "some very sensitive info about the server, like OS version: "
+           (System/getProperty "os.version")))
